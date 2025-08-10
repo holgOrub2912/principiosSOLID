5704f59c7eb6484af210bf3ded2a184942ed785b
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-08-10 10:39:19 -0500

    Reorganizar clases relacionadas con Ingrediente

--- proyecto_Antiguo/Isas_Pizza/Ingrediente.cs
+++ proyecto_Antiguo/Isas_Pizza/Ingrediente.cs
@@ -3,48 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Isas_Pizza
 {
-    public class Ingrediente
+    public enum Unidad
     {
-        public string nombre {get;}
-        public DateTime fechaVencimiento {get;}
-        public double peso {get;}
-        public string descripcion {get;}
+        UNIDAD,
+        LIBRA,
+        GRAMO,
+        LITRO,
+        MILILITRO,
+    }
 
-        public static void ValidateArgs(string nombre,
-                                        DateTime fechaVencimiento,
-                                        double peso,
-                                        string descripcion)
+    public static class UnidadExtensions
+    {
+        public static string GetString(this Unidad unidad, bool singular)
+            => (unidad, singular) switch
         {
-            if (string.IsNullOrEmpty(nombre))
-                throw new ArgumentException(
-                    "Nombre del ingrediente no puede ser nulo",
-                    nameof(nombre)
-                );
-            if (fechaVencimiento < DateTime.Today)
-                throw new ArgumentOutOfRangeException(
-                    nameof(fechaVencimiento),
-                    "La fecha de vencimiento no puede estar en el pasado"
-                );
-            if (peso <= 0.0)
-                throw new ArgumentOutOfRangeException(
-                    nameof(peso),
-                    "El peso debe de ser un valor positivo"
-                );
-        }
+            // Manejar casos de pluralidad especiales.
+            (Unidad.UNIDAD, false) => "unidades",
+            (_, true) => unidad.ToString("G").ToLower(),
+            (_, false) => unidad.ToString("G").ToLower() + "s",
+        };
+    }
+    
+    public record Ingrediente
+    {
+        [Required(ErrorMessage = "{0} no debe estar vacío")]
+        public string nombre {get; init;}
+        public string descripcion {get; init;}
+        public Unidad unidad {get; init;}
+    }
 
-        public Ingrediente(string nombre,
-                           DateTime fechaVencimiento,
-                           double peso,
-                           string descripcion)
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class UnidadCantidadEnteraAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object ingCantidadObj)
         {
-            ValidateArgs(nombre, fechaVencimiento, peso, descripcion);
-            this.nombre = nombre;
-            this.fechaVencimiento = fechaVencimiento;
-            this.peso = peso;
-            this.descripcion = descripcion;
+            IngredienteCantidad ingCant = (IngredienteCantidad) ingCantidadObj;
+            return ingCant.ingrediente.unidad != Unidad.UNIDAD ||
+                   ingCant.cantidad == (int) ingCant.cantidad;
         }
+    }
+
+    [UnidadCantidadEntera(ErrorMessage = "cantidad debe ser especificado en unidades enteras")]
+    public record IngredienteCantidad
+    {
+        public Ingrediente ingrediente {get; init;}
+        [Range(0.0, Double.MaxValue, ErrorMessage = "{0} no puede ser negativo")]
+        public double cantidad {get; init;}
+
+        public static ValidationResult validarCantidadUnidades(
+            IngredienteCantidad ingCant
+        ) =>   (ingCant.ingrediente.unidad != Unidad.UNIDAD ||
+                ingCant.cantidad == (int) ingCant.cantidad)
+             ? ValidationResult.Success
+             : new ValidationResult("Cantidad inválida");
+    }
+
+    public record IngredienteEnStock: IngredienteCantidad
+    {
+        [CustomValidation(typeof(IngredienteEnStock), "ValidarFechaFutura")]
+        public DateTime fechaVencimiento {get; init;}
+
+        public static ValidationResult ValidarFechaFutura(DateTime fecha)
+            => fecha > DateTime.Today
+             ? ValidationResult.Success
+             : new ValidationResult($"{nameof(fecha)} debe de estar en el futuro");
     }
 }
--- proyecto_Antiguo/Isas_PizzaTests/IngredienteTests.cs
+++ proyecto_Antiguo/Isas_PizzaTests/IngredienteTests.cs
@@ -1,37 +1,195 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 using Isas_Pizza;
+using System.ComponentModel.DataAnnotations;
 
 namespace Isas_PizzaTests
 {
     public class IngredienteTests 
     {
+        private readonly ITestOutputHelper output;
+
+        public IngredienteTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         private readonly TimeSpan timediff = new TimeSpan(20, 0, 0, 0, 0);
 
-        [Fact]
-        public void NombreVacioFalla()
-        {
-            Assert.Throws<ArgumentException>("nombre",
-                () => new Ingrediente("", new DateTime(1900, 3, 4), 2.0, "Libras de tomate.")
-            );
-        }
-
-        [Fact]
-        public void FechaVencimientoPasadaFalla()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>("fechaVencimiento",
-                () => new Ingrediente("Tomate", DateTime.Today.Subtract(timediff), 2.0, "Libras de tomate.")
-            );
-        }
 
         [Theory]
-        [InlineData(-10.0)]
-        [InlineData(0.0)]
-        public void PesoInvalidoFalla(double val)
+        [InlineData(Unidad.UNIDAD, true, "unidad")]
+        [InlineData(Unidad.UNIDAD, false, "unidades")]
+        [InlineData(Unidad.LIBRA, true, "libra")]
+        [InlineData(Unidad.LIBRA, false, "libras")]
+        [InlineData(Unidad.GRAMO, true, "gramo")]
+        [InlineData(Unidad.GRAMO, false, "gramos")]
+        [InlineData(Unidad.LITRO, true, "litro")]
+        [InlineData(Unidad.LITRO, false, "litros")]
+        public void UnidadesConvertidasCorrectamente(Unidad unidad,
+                                                     bool singular,
+                                                     string expected)
+            => Assert.Equal(expected, unidad.GetString(singular));
+
+        [Fact]
+        public void ValidacionNombreVacioFalla()
         {
-            Assert.Throws<ArgumentOutOfRangeException>("peso",
-                () => new Ingrediente("Tomate", DateTime.Today.Add(timediff), val, "Libras de tomate.")
+            Ingrediente ingrediente = new Ingrediente
+            {
+                nombre = "",
+                descripcion = "Tomate alino",
+                unidad = Isas_Pizza.Unidad.UNIDAD
+            };
+
+            output.WriteLine(ingrediente.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingrediente,
+                                   new ValidationContext(ingrediente),
+                                   resultadosValidacion);
+
+            Assert.Equal(
+                "nombre no debe estar vacío",
+                resultadosValidacion[0].ErrorMessage
             );
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void ValidacionNombreLlenoPasa()
+        {
+            Ingrediente ingrediente = new Ingrediente
+            {
+                nombre = "Tomate",
+                descripcion = "",
+                unidad = Isas_Pizza.Unidad.UNIDAD
+            };
+
+            output.WriteLine(ingrediente.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingrediente,
+                                   new ValidationContext(ingrediente),
+                                   resultadosValidacion);
+
+            Assert.Empty(resultadosValidacion);
+            Assert.True(isValid);
+        }
+        
+        [Fact]
+        public void ValidacionIngredienteCantidadNegativaFalla()
+        {
+            IngredienteCantidad ingredienteCantidad = new IngredienteCantidad{
+                ingrediente = new Ingrediente
+                    {
+                        nombre = "Ajo",
+                        descripcion = "Diente de ajo",
+                        unidad = Unidad.UNIDAD,
+                    },
+                cantidad = -30.0
+            };
+
+            output.WriteLine(ingredienteCantidad.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingredienteCantidad,
+                                   new ValidationContext(ingredienteCantidad),
+                                   resultadosValidacion, true);
+
+            Assert.Equal(
+                "cantidad no puede ser negativo",
+                resultadosValidacion[0].ErrorMessage
+            );
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void ValidacionIngredienteCantidadPositivaPasa()
+        {
+            IngredienteCantidad ingredienteCantidad = new IngredienteCantidad
+            {
+                ingrediente = new Ingrediente
+                    {
+                        nombre = "Ajo",
+                        descripcion = "Diente de ajo",
+                        unidad = Unidad.UNIDAD,
+                    },
+                cantidad = 20.0
+            };
+
+            output.WriteLine(ingredienteCantidad.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingredienteCantidad,
+                                   new ValidationContext(ingredienteCantidad),
+                                   resultadosValidacion, true);
+
+            Assert.Empty(resultadosValidacion);
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void ValidacionIngredienteUnidadesDecimalesFallan()
+        {
+            IngredienteCantidad ingredienteCantidad = new IngredienteCantidad
+            {
+                ingrediente = new Ingrediente
+                    {
+                        nombre = "Ajo",
+                        descripcion = "Diente de ajo",
+                        unidad = Unidad.UNIDAD,
+                    },
+                cantidad = 34.2
+            };
+
+            output.WriteLine(ingredienteCantidad.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingredienteCantidad,
+                                   new ValidationContext(ingredienteCantidad),
+                                   resultadosValidacion, true);
+
+            Assert.Equal(
+                "cantidad debe ser especificado en unidades enteras",
+                resultadosValidacion[0].ErrorMessage
+            );
+            Assert.False(isValid);
+        }
+        
+        [Fact]
+        public void ValidacionFechaVencimientoPasadaFalla()
+        {
+            IngredienteEnStock ingEnStock = new IngredienteEnStock
+            {
+                ingrediente = new Ingrediente
+                    {
+                        nombre = "Ajo",
+                        descripcion = "Diente de ajo",
+                        unidad = Unidad.UNIDAD,
+                    },
+                cantidad = 5.0,
+                fechaVencimiento = DateTime.Today.Subtract(timediff),
+            };
+
+            output.WriteLine(ingEnStock.ToString());
+
+            List<ValidationResult> resultadosValidacion = new();
+            bool isValid = Validator
+                .TryValidateObject(ingEnStock,
+                                   new ValidationContext(ingEnStock),
+                                   resultadosValidacion, true);
+
+            Assert.Equal(
+                "fecha debe de estar en el futuro",
+                resultadosValidacion[0].ErrorMessage
+            );
+            Assert.False(isValid);
         }
     }
 }
