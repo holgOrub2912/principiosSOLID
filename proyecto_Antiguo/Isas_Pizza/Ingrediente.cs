4e2f5b96c345430e2e114aeeebc1b46718eb2c90
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-08-10 12:33:39 -0500

    Añadir documentación

--- proyecto_Antiguo/Isas_Pizza/Ingrediente.cs
+++ proyecto_Antiguo/Isas_Pizza/Ingrediente.cs
@@ -7,6 +7,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Isas_Pizza
 {
+    /// <summary>
+    /// Unidad con la que se mide un ingrediente.
+    /// </summary>
     public enum Unidad
     {
         UNIDAD,
@@ -16,8 +19,20 @@ namespace Isas_Pizza
         MILILITRO,
     }
 
+    /// <summary>
+    /// Clase extensión de Unidades, implementando operaciones útiles
+    /// para manipularlas.
+    /// </summary>
     public static class UnidadExtensions
     {
+        /// <summary>
+        /// Convertir unidad en string.
+        /// </summary>
+        /// <param name="unidad">Unidad a convertir.</param>
+        /// <param name="singular">
+        ///     Si la unidad debe estar en singular (true) o plural (false).
+        /// </param>
+        /// <returns>Representación de la unidad como string.</returns>
         public static string GetString(this Unidad unidad, bool singular)
             => (unidad, singular) switch
         {
@@ -28,14 +43,24 @@ namespace Isas_Pizza
         };
     }
     
-    public record Ingrediente
+    /// <summary>
+    /// Tipo de ingrediente registrado.
+    /// </summary>
+    public record class Ingrediente
     {
+        /// <summary>Nombre del tipo de ingrediente.</summary>
         [Required(ErrorMessage = "{0} no debe estar vacío")]
         public string nombre {get; init;}
+        /// <summary>Descripción del tipo de ingrediente.</summary>
         public string descripcion {get; init;}
+        /// <summary>Unidades con las que se mide el ingrediente.</summary>
         public Unidad unidad {get; init;}
     }
 
+    /// <summary>
+    /// Validador de si un IngredienteCantidad medido en unidades tiene
+    /// una cantidad entera.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class UnidadCantidadEnteraAttribute : ValidationAttribute
     {
@@ -47,26 +72,37 @@ namespace Isas_Pizza
         }
     }
 
+    /// <summary>
+    /// Cantidad determinada para un ingrediente.
+    /// </summary>
     [UnidadCantidadEntera(ErrorMessage = "cantidad debe ser especificado en unidades enteras")]
-    public record IngredienteCantidad
+    public record class IngredienteCantidad
     {
+        /// <summary>
+        /// Tipo de ingrediente.
+        /// </summary>
         public Ingrediente ingrediente {get; init;}
+        /// <summary>
+        /// Cantidad (medida en ingrediente.unidad) del ingrediente.
+        /// </summary>
         [Range(0.0, Double.MaxValue, ErrorMessage = "{0} no puede ser negativo")]
         public double cantidad {get; init;}
-
-        public static ValidationResult validarCantidadUnidades(
-            IngredienteCantidad ingCant
-        ) =>   (ingCant.ingrediente.unidad != Unidad.UNIDAD ||
-                ingCant.cantidad == (int) ingCant.cantidad)
-             ? ValidationResult.Success
-             : new ValidationResult("Cantidad inválida");
     }
 
-    public record IngredienteEnStock: IngredienteCantidad
+    /// <summary>
+    /// Representación de la cantidad en existencia de un ingrediente en el inventario.
+    /// </summary>
+    public record class IngredienteEnStock: IngredienteCantidad
     {
+        /// <summary>
+        /// Fecha en la que se vence el ingrediente.
+        /// </summary>
         [CustomValidation(typeof(IngredienteEnStock), "ValidarFechaFutura")]
         public DateTime fechaVencimiento {get; init;}
 
+        /// <summary>
+        /// Validador de si fechaVencimiento está en el futuro.
+        /// </summary>
         public static ValidationResult ValidarFechaFutura(DateTime fecha)
             => fecha > DateTime.Today
              ? ValidationResult.Success
